51edb02f0a87519533767fcda47f86eadf4bbdf4
avv73 <a.v.v73@abv.bg>
2018-05-26 00:19:40 +0300

    initial commitment
    - added provided skeleton code from the main exercise
    - added documentation
    - part 2 from the first assignment completed

---
+++ 07. CSharp-OOP-Basics-Workshop-Skeleton/Forum.Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forum.Models
+{
+    public class Category
+    {
+        public Category(int id, string name, IEnumerable<int> posts)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Posts = new List<int>(posts);
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<int> Posts { get; set; }
+
+    }
+
+}
+
+
---
+++ 07. CSharp-OOP-Basics-Workshop-Skeleton/Forum.Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Forum.Models
+{
+    public class User
+    {
+        public User(int id, string userName, string password, IEnumerable<int> postIds)
+        {
+            this.Id = id;
+            this.UserName = userName;
+            this.Password = password;
+            this.PostIds = new List<int>(postIds);
+        }
+
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public ICollection<int> PostIds { get; set; }
+    } 
+}
+
